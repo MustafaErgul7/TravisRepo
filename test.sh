e8972702283678c7a9e7cc25dda16a57aeff87a3
mustafa-travisci <mustafa@travis-ci.org>
2022-04-22 17:25:47 +0300

    test env

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
-VERSION="7.4.27"
+#VERSION="7.4.27"
+#
+#if [[ ! $VERSION =~ ^master$ ]] && [[ "$(printf "7.4\n$VERSION" | sort -V | head -n1)" < "7.4" ]]; then
+#    echo "werwerwer"
+#else
+#    echo "thi"
+#    echo $VERSION
+#fi
 
-if [[ ! $VERSION =~ ^master$ ]] && [[ "$(printf "7.4\n$VERSION" | sort -V | head -n1)" < "7.4" ]]; then
-    echo "werwerwer"
-else
-    echo "thi"
-    echo $VERSION
-fi
+if [ -n "$test"]; then echo "test env is here"; fi
+if [ -n "$test2"]; then echo "test2 env is here"; fi
